689a5254e7b389e083cebe05fe043cfd63f9ffa1
cgomezlop95 <c.gomezlop95@gmail.com>
2024-03-01 11:50:00 +0100

    Login and signup form modified

--- routes/auth.js
+++ routes/auth.js
@@ -54,30 +54,4 @@ router.get(
   }
 );
 
-//Log out functionality
-
-router.get("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/login");
-  });
-});
-
-//Google login
-
-router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req, res) => {
-    res.redirect("/profile");
-  }
-);
-
 module.exports = router;
